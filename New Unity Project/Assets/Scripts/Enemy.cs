f66a191224f579fa992fcd8101a8ed22399e62c9
Robinwolfy <46539332+Robinwolfy@users.noreply.github.com>
2019-03-11 00:02:32 -0400

    Challenge2

---
+++ New Unity Project/Assets/Scripts/Enemy.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Transforms to act as start and end markers for the journey.
+    public Transform startMarker;
+    public Transform endMarker;
+
+    // Movement speed in units/sec.
+    private bool facingRight;
+    public float speed = 1.0F;
+    public bool forward;
+    //bool reverse;
+
+    // Time when the movement started.
+    private float startTime;
+
+    // Total distance between the markers.
+    private float journeyLength;
+
+    void Start()
+    {
+        // Keep a note of the time the movement started.
+        startTime = Time.time;
+        forward = true;
+
+        // Calculate the journey length.
+        journeyLength = Vector2.Distance(startMarker.position, endMarker.position);
+    }
+
+    // Follows the target position like with a spring
+    void Update()
+    {
+
+        if (forward)
+        {
+            float step = speed * Time.deltaTime; // calculate distance to move
+            transform.position = Vector2.MoveTowards(transform.position, endMarker.position, step);
+        }
+        if (!forward)
+        {
+            float step = speed * Time.deltaTime; // calculate distance to move
+            transform.position = Vector2.MoveTowards(transform.position, startMarker.position, step);
+        }
+
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("End"))
+        {
+            forward = false;
+        }
+        if (other.gameObject.CompareTag("Start"))
+        {
+            forward = true;
+        }
+
+        facingRight = !facingRight;
+
+        Vector3 theScale = transform.localScale;
+
+        theScale.x *= -1;
+
+        transform.localScale = theScale;
+    }
+}
+
--- New Unity Project/Assets/Scripts/PlayerController.cs
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -1,38 +1,193 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb2d;
-    public float speed;
+    bool grounded;
+    bool facingRight;
+    bool canInput;
+    bool win;
+    Animator anim;
+    float speed;
+    float nextStage;
+    AudioSource playerSound;
+    public AudioClip clip2;
+
+    
+    public float moveSpeed;
     public float jump;
+    public float delay;
+    public int lives;
+    public int score;
+    public Text scoreText;
+    public Text winText;
+    public Text lifeText;
+    public Transform lvl2;
+    public int goal;
+    public int goal2;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        facingRight = true;
+        lives = 3;        
+        winText.text = "Collect The Burritos!";
+        canInput = true;
+        playerSound = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        anim.SetBool("OnGround", true);
+
         float moveHorizontal = Input.GetAxis("Horizontal");
 
-        Vector2 movement = new Vector2(moveHorizontal,0);
+        Vector2 movement = new Vector2(moveHorizontal, 0);
 
-        rb2d.AddForce(movement * speed);
+        if (canInput && grounded)
+        {
+            rb2d.AddForce(movement * moveSpeed);
+        }
+
+        if (moveHorizontal != 0 && canInput)
+        {
+            winText.text = "";
+        }
+
+        speed = rb2d.velocity.magnitude;
+
+
+        if (!grounded)
+        { 
+            anim.SetBool("OnGround", false);
+        }
+        if (speed > 0)
+        {
+            anim.SetInteger("State", 1);
+            anim.speed = speed/3;
+        }    
+        if (speed == 0)
+        {
+            anim.SetInteger("State", 0);
+            anim.speed = 1;
+        }
+
+        Flip(moveHorizontal);
+
+    }
+    private void Update()
+    {
+        if (lives < 1)
+        {
+            this.gameObject.SetActive(false);
+            winText.text = ";_; You Lose ;_;";
+        }
+        AddScore();
     }
 
     void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.collider.tag == "Ground")
         {
-            if (Input.GetKey(KeyCode.UpArrow))
+            grounded = true;
+            if (Input.GetKeyDown(KeyCode.UpArrow) && canInput)
             {
                 rb2d.AddForce(new Vector2(0, jump), ForceMode2D.Impulse);
-            }
+                anim.SetInteger("State", 2);                
+            }            
 
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            grounded = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "Ground")
+        {
+            grounded = true;
+            anim.SetTrigger("Land");
+        }
+        if(collision.collider.tag == "Bad")
+        {
+            lives --;
+            collision.gameObject.SetActive(false);
+            AddScore();
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Good"))
+        {
+            score ++;
+            collision.gameObject.SetActive(false);
+            AddScore();
+        }
+    }
+
+    private void Flip(float moveHorizontal)
+    {
+        if (moveHorizontal > 0 && !facingRight || moveHorizontal < 0 && facingRight)
+        {
+            facingRight = !facingRight;
+
+            Vector3 theScale = transform.localScale;
+
+            theScale.x *= -1;
+
+            transform.localScale = theScale;
+        }
+    }
+    private void AddScore()
+    {
+        scoreText.text = "x " + score.ToString();
+        lifeText.text = "x " + lives.ToString();
+
+        if (score >= goal && !win)
+        {
+            winText.text = "Stage Cleared!";
+            canInput = false;
+            Win();
+            playerSound.clip = clip2;
+            playerSound.Play();
+
+        }
+        else if (score >= goal && win)
+        {
+            winText.text = "You Win!";
+            canInput = false;            
+        }
+        
+    }
+
+    private void Win()
+    {
+        Debug.Log("win");
+        nextStage += Time.deltaTime;
+        Debug.Log(nextStage);
+        if (nextStage >= delay)
+        {
+            Debug.Log("Next");
+            this.transform.position = lvl2.position;
+            goal = goal + goal2;
+            win = true;
+            winText.text = "";
+            canInput = true;
+            lives = 3;
         }
     }
 }
